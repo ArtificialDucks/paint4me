6314e664a22417d32017a82dd1b3344da18d18c8
joeyt <jktavitas@gmail.com>
2018-10-11 02:28:06 -0500

    Canvas & Current Color Indicators added

---
+++ Painter/Assets/Painting Keys and Script/ColorIndicator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorIndicator : MonoBehaviour {
+
+	public ColorChange color;
+	private MeshRenderer r;
+	// Use this for initialization
+	void Start () {
+		r = GetComponent<MeshRenderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		r.material.color = color.myColor;
+	}
+}
