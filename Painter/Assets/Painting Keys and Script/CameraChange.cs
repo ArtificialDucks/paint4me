0ac3df63bdeac59343c5efc9983b4be8802138d6
Justin Halter <jehalter@gmail.com>
2018-10-22 19:00:39 -0500

    10/22/18
    camera scrolling

--- Painter/Assets/Painting Keys and Script/CameraChange.cs
+++ Painter/Assets/Painting Keys and Script/CameraChange.cs
@@ -9,10 +9,16 @@ public class CameraChange : MonoBehaviour {
     public List<GameObject> QuadrantControllers = new List<GameObject>();
     UpperQuadrantControl QuadScript;
 
+    //camera locations to be used with scroll wheel
+    public List<GameObject> ScrollWheelLocations = new List<GameObject>();
     CameraPosition pos;
     public float speed;
     private float step;
     private Transform EndLocation;
+    private int ScrollLocation;
+    
+    //control whether mouse can scroll or not
+    public bool CanZoom;
 
     public int CurrentPainting;
     // Use this for initialization
@@ -21,10 +27,31 @@ public class CameraChange : MonoBehaviour {
         EndLocation = pos.transform;
         CurrentPainting = 0;
         QuadScript = QuadrantControllers[CurrentPainting].GetComponent<UpperQuadrantControl>();
+        ScrollLocation = 0;
+        CanZoom = true;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        //mouse wheel to scroll through paint/canvas/workshop view
+        if (CanZoom == true)
+        {
+            var d = Input.GetAxis("Mouse ScrollWheel");
+            //scroll up
+            if (d > 0f)
+            {
+                if (ScrollLocation > 0) ScrollLocation--;
+                EndLocation.position = ScrollWheelLocations[ScrollLocation].transform.position;
+            }
+            //scroll down
+            else if (d < 0f)
+            {
+                if (ScrollLocation <= 1) ScrollLocation++;
+                EndLocation.position = ScrollWheelLocations[ScrollLocation].transform.position;
+            }
+        }
+        
+
         step = speed * Time.deltaTime;
         if (Input.GetKeyDown("down"))
         {
@@ -65,7 +92,7 @@ public class CameraChange : MonoBehaviour {
                 ActivateQuadrant(pos.QuadValue);
             }
         }
-        transform.position = Vector3.MoveTowards(transform.position, EndLocation.position, step);
+        transform.position = Vector3.MoveTowards(transform.position, EndLocation.position, step*3.0f);
     }
 
     void ActivateQuadrant(int val)
