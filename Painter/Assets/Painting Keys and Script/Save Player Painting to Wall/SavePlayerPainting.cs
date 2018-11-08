26af4a34508a96b43f5d1de7fd4324ec0d19f7c8
Halter <JHALTER2@depaul.edu>
2018-11-08 13:21:28 -0600

    more animation tests

---
+++ Painter/Assets/Painting Keys and Script/Save Player Painting to Wall/SavePlayerPainting.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SavePlayerPainting : MonoBehaviour {
+    public List<GameObject> WorkshopCanvasLocations = new List<GameObject>();
+    public GameObject CurrentPainting;
+    private int CurrentCanvas;
+    // Use this for initialization
+    void Start () {
+        CurrentCanvas = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void SavePainting(GameObject painting)
+    {
+        CurrentPainting = painting;
+    }
+
+    void DisplayPainting(GameObject painting)
+    {
+
+    }
+}
